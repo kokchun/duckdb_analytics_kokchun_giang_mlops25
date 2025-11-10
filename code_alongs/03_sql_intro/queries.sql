913eccc70722486b31ebd96130c0ba952cb1a915
kokchun <kokchungiang@gmail.com>
2025-11-10 15:13:21 +0100

    queries

--- code_alongs/03_sql_intro/queries.sql
+++ code_alongs/03_sql_intro/queries.sql
@@ -1,1 +1,22 @@
-SELECT * FROM funny_jokes;
+SELECT
+    *
+FROM
+    funny_jokes;
+
+-- sort by rating ascending order
+SELECT
+    *
+FROM
+    funny_jokes
+ORDER BY
+    rating;
+
+-- sort by rating descending order
+SELECT
+    *
+FROM
+    funny_jokes
+ORDER BY
+    rating
+DESC;
+
