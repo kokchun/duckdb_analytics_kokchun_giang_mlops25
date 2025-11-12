76ca06ac1ac6bab4fbe07135f8b2a230d4ad62ec
kokchun <kokchungiang@gmail.com>
2025-11-12 13:27:31 +0100

    query select a few columns

--- code_alongs/04_querying/sql/query_salaries.sql
+++ code_alongs/04_querying/sql/query_salaries.sql
@@ -1,6 +1,34 @@
-SELECT * FROM data_jobs;
+SELECT
+    *
+FROM
+    data_jobs;
 
 -- case insensitive in SQL 
 -- by convention UPPERCASE for SQL commands
 -- lowercase for columns
-SELECT * FROM data_jobs LIMIT 5;
+SELECT
+    *
+FROM
+    data_jobs
+LIMIT
+    5;
+
+-- offset 3
+SELECT
+    *
+FROM
+    data_jobs
+OFFSET
+    3
+LIMIT
+    5;
+
+-- pick out a few columns
+SELECT
+    work_year,
+    experience_level,
+    job_title,
+    salary_in_usd,
+    company_size
+FROM 
+    data_jobs;
