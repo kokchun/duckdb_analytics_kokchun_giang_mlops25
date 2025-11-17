77a18c434399902961ad2fb7c278df965160b72d
kokchun <kokchungiang@gmail.com>
2025-11-17 10:39:47 +0100

    crud insert data

--- code_alongs/06_crud/sql/1_crud_insert.sql
+++ code_alongs/06_crud/sql/1_crud_insert.sql
@@ -3,3 +3,11 @@ INSERT INTO
     (word, description)
 VALUES
     ('query', 'request made to a database to retrieve, modify or manipulate data');
+
+
+INSERT INTO
+    database_schema.sql_table
+    (word, description)
+VALUES
+    ('CRUD', 'create, read, update, delete - basic actions to be performed on data'),
+	('range condition', 'filter data within a specific range of values');
