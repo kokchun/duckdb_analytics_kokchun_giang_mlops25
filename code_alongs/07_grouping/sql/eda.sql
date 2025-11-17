8f25f086f60c0681b4c3d7cf9bbefb0158dcbe84
kokchun <kokchungiang@gmail.com>
2025-11-17 13:23:14 +0100

    eda init

---
+++ code_alongs/07_grouping/sql/eda.sql
@@ -0,0 +1,15 @@
+/* 
+EDA - Exploratory Data Analysis
+
+- explore a dataset with purpose to understand it
+- find some insights 
+
+ */
+
+ FROM food;
+
+ SELECT DISTINCT id FROM food;
+
+ SELECT DISTINCT id FROM food ORDER BY id;
+
+ 
