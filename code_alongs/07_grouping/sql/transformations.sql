d646dbe04620711a5c9b95a7686e111a4b17b375
kokchun <kokchungiang@gmail.com>
2025-11-17 13:43:38 +0100

    transformed into cleaned food data

--- code_alongs/07_grouping/sql/transformations.sql
+++ code_alongs/07_grouping/sql/transformations.sql
@@ -13,9 +13,18 @@ Very good to SELECT before doing the transformation
 SELECT
    id AS food,
    value AS number_of_searches,
-   week_id[1:4] AS year,
+   week_id[1:4] AS year, --similar to python slicing
    week_id[-2:] AS week
 FROM 
     food;
 
-
+-- create a new table with these transformations
+CREATE TABLE IF NOT EXISTS cleaned_food AS (
+    SELECT
+        id AS food,
+        value AS number_of_searches,
+        week_id[1:4] AS year,
+        week_id[-2:] AS week
+    FROM 
+        food
+);
