1d4a5f0e6ab1d2c1426c5bc1fab3b6d35c78cbef
kokchun <kokchungiang@gmail.com>
2025-11-17 14:23:29 +0100

    group by lect done

--- code_alongs/07_grouping/sql/groupby.sql
+++ code_alongs/07_grouping/sql/groupby.sql
@@ -21,3 +21,31 @@ GROUP BY year
 HAVING 
     total_searches > 400_000
 ORDER BY year; 
+
+
+-- can group by several columns
+/* 
+works like this:
+groups by food and year
+for this group there are 52 rows
+take the sum of these 52 rows
+ */
+SELECT 
+    food, 
+    year, 
+    SUM(number_of_searches) AS total_searches
+FROM cleaned_food
+GROUP BY
+    food, year
+ORDER BY food, year;
+
+
+SELECT 
+    food, 
+    year, 
+    SUM(number_of_searches) AS total_searches
+FROM cleaned_food
+GROUP BY
+    food, year
+HAVING food in ('pizza', 'sushi')
+ORDER BY food, year;
