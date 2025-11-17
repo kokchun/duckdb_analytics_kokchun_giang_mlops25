c7fd32214aa5b4d1dbfefbd380742e0720043ebc
kokchun <kokchungiang@gmail.com>
2025-11-17 13:39:11 +0100

    before transformations

--- code_alongs/07_grouping/sql/transformations.sql
+++ code_alongs/07_grouping/sql/transformations.sql
@@ -6,4 +6,16 @@ Based on the EDA, I want to do the following
 - value -> number_of_searches
 - week_id -> week column and year column
 
- */
+Very good to SELECT before doing the transformation
+
+*/
+
+SELECT
+   id AS food,
+   value AS number_of_searches,
+   week_id[1:4] AS year,
+   week_id[-2:] AS week
+FROM 
+    food;
+
+
