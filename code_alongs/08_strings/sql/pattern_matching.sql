8b83dfc7f3645c244dce5cfb52d39cee104fe729
kokchun <kokchungiang@gmail.com>
2025-11-19 13:36:31 +0100

    some pattern matching

---
+++ code_alongs/08_strings/sql/pattern_matching.sql
@@ -0,0 +1,25 @@
+-- LIKE operator
+SELECT 
+    *
+FROM 
+    refined.sql_glossary
+WHERE
+    sql_word LIKE 'ORD%';
+
+-- match all that ends with TE
+SELECT 
+    *
+FROM 
+    refined.sql_glossary
+WHERE
+    sql_word LIKE '%TE';
+
+-- match all that ends with te case insensitive
+SELECT 
+    *
+FROM 
+    refined.sql_glossary
+WHERE
+    sql_word ILIKE '%te';
+
+    
