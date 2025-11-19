cd57188d360000708fac90efce49bdcd006e8e2a
kokchun <kokchungiang@gmail.com>
2025-11-19 11:28:21 +0100

    string functions

--- code_alongs/08_strings/sql/string_functions.sql
+++ code_alongs/08_strings/sql/string_functions.sql
@@ -5,12 +5,18 @@
 TRIM() in SQL
 python -> strip() # remove leading and trailing 
  */
-
 SELECT
     sql_word,
     UPPER(TRIM(sql_word)) AS cleaned_word,
     LOWER(TRIM(sql_word)) AS cleaned_word_lower,
     'SQL command: ' || cleaned_word_lower AS concatenation
-FROM 
+FROM
     staging.glossary;
 
+-- leading and trailing space
+-- > 1 space between some words
+SELECT
+    description,
+    REPLACE (TRIM(description), '  ', ' ') AS cleaned_description
+FROM
+    staging.glossary;
