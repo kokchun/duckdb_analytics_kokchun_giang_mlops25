abbcbd4899b75b939c651c2b53527ab1685e3b73
kokchun <kokchungiang@gmail.com>
2025-11-19 11:22:52 +0100

    some string functions

--- code_alongs/08_strings/sql/ingest_glossary.sql
+++ code_alongs/08_strings/sql/ingest_glossary.sql
@@ -1,1 +1,10 @@
--- ingest data into a duckdb database
+
+CREATE SCHEMA IF NOT EXISTS staging; 
+
+-- schema.table
+CREATE TABLE IF NOT EXISTS staging.glossary AS (
+    SELECT * FROM 'data/sql_terms.csv'
+);
+
+
+-- duckdb data/glossary.duckdb < sql/ingest_glossary.sql
---
+++ code_alongs/08_strings/sql/string_functions.sql
@@ -0,0 +1,16 @@
+/* sql_word
+- leading and trailing spaces
+- mix of upper and lowercases
+
+TRIM() in SQL
+python -> strip() # remove leading and trailing 
+ */
+
+SELECT
+    sql_word,
+    UPPER(TRIM(sql_word)) AS cleaned_word,
+    LOWER(TRIM(sql_word)) AS cleaned_word_lower,
+    'SQL command: ' || cleaned_word_lower AS concatenation
+FROM 
+    staging.glossary;
+
