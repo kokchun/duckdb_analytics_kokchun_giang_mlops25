37bb0123979ab20e40e2d331f703d2968eb4ecd8
kokchun <kokchungiang@gmail.com>
2025-11-24 11:35:06 +0100

    set operations init

--- code_alongs/11_set_operations/sql/set_operations_sakila.sql
+++ code_alongs/11_set_operations/sql/set_operations_sakila.sql
@@ -1,11 +1,31 @@
+-- check customer
 SELECT 
     'customer' as type,
     c.first_name,
     c.last_name
 FROM customer c; 
 
+-- check actor
 SELECT 
     'actor' as type,
     a.first_name,
     a.last_name
 FROM actor a; 
+
+-- select all customers and all actors starting with A in firstname
+SELECT 
+    'customer' as type,
+    c.first_name,
+    c.last_name
+FROM customer c
+WHERE
+    c.first_name ILIKE 'a%'
+UNION
+SELECT 
+    'actor' as type,
+    a.first_name,
+    a.last_name
+FROM actor a
+WHERE 
+    a.first_name ILIKE 'a%';
+
