3b814dcba6270cb4ae2385f2384d615b8d854c9e
kokchun <kokchungiang@gmail.com>
2025-11-24 11:43:07 +0100

    set operations on sakila done

--- code_alongs/11_set_operations/sql/set_operations_sakila.sql
+++ code_alongs/11_set_operations/sql/set_operations_sakila.sql
@@ -29,3 +29,28 @@ FROM actor a
 WHERE 
     a.first_name ILIKE 'a%';
 
+-- find all overlapping names between actors and customers
+SELECT 
+    c.first_name,
+    c.last_name
+FROM customer c; 
+INTERSECT
+SELECT 
+    a.first_name,
+    a.last_name
+FROM actor a; 
+
+-- find all initials JD in both customer and actor 
+SELECT 
+    'customer' AS type,
+    c.first_name,
+    c.last_name
+FROM customer c
+WHERE c.first_name LIKE 'J%' AND c.last_name LIKE 'D%'
+UNION ALL
+SELECT 
+    'actor' AS type,
+    a.first_name,
+    a.last_name
+FROM actor a
+WHERE a.first_name LIKE 'J%' AND a.last_name LIKE 'D%';
